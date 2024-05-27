225bb0467785485f22c5dd7d463965a19401e20b
= <=>
2024-05-27 16:05:48 +0700

    update UI list chat

--- src/components/ChatList/chat-list.tsx
+++ src/components/ChatList/chat-list.tsx
@@ -4,7 +4,141 @@ import "./chat-list.scss"
 function ChatList() {
     return (
         <div className="chat-list">
+<<<<<<< Updated upstream
             <h1>Hello1</h1>
+=======
+            <div className="chat-list__header">
+                <div className="chat-list__header-user">
+                    <img src={typing} alt="avatar"/>
+
+                    <div className="info">
+                        <h4>My Name</h4>
+                        <p className="status">Status</p>
+                    </div>
+                </div>
+
+                <div className="chat-list__header-icons">
+                    <i className="bi bi-three-dots"></i>
+                </div>
+            </div>
+
+            <div className="chat-list__search">
+                <input type="text" placeholder="Search on Chat"/>
+            </div>
+
+            <div className="chat-list__content">
+                <div className="chat-list__content-user">
+                    <img src={typing} alt="avatar"/>
+                    <div className="info-message">
+                        <div className="info">
+                            <h4>Name</h4>
+                        </div>
+                        <div className="message">
+                            <p>You: Chào Giang</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="chat-list__content-user">
+                    <img src={typing} alt="avatar"/>
+                    <div className="info-message">
+                        <div className="info">
+                            <h4>Name</h4>
+                        </div>
+                        <div className="message">
+                            <p>You: Chào Giang</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="chat-list__content-user">
+                    <img src={typing} alt="avatar"/>
+                    <div className="info-message">
+                        <div className="info">
+                            <h4>Name</h4>
+                        </div>
+                        <div className="message">
+                            <p>You: Chào Giang</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="chat-list__content-user">
+                    <img src={typing} alt="avatar"/>
+                    <div className="info-message">
+                        <div className="info">
+                            <h4>Name</h4>
+                        </div>
+                        <div className="message">
+                            <p>You: Chào Giang</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="chat-list__content-user">
+                    <img src={typing} alt="avatar"/>
+                    <div className="info-message">
+                        <div className="info">
+                            <h4>Name</h4>
+                        </div>
+                        <div className="message">
+                            <p>You: Chào Giang</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="chat-list__content-user">
+                    <img src={typing} alt="avatar"/>
+                    <div className="info-message">
+                        <div className="info">
+                            <h4>Name</h4>
+                        </div>
+                        <div className="message">
+                            <p>You: Chào Giang</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="chat-list__content-user">
+                    <img src={typing} alt="avatar"/>
+                    <div className="info-message">
+                        <div className="info">
+                            <h4>Name</h4>
+                        </div>
+                        <div className="message">
+                            <p>You: Chào Giang</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="chat-list__content-user">
+                    <img src={typing} alt="avatar"/>
+                    <div className="info-message">
+                        <div className="info">
+                            <h4>Name</h4>
+                        </div>
+                        <div className="message">
+                            <p>You: Chào Giang</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="chat-list__content-user">
+                    <img src={typing} alt="avatar"/>
+                    <div className="info-message">
+                        <div className="info">
+                            <h4>Name</h4>
+                        </div>
+                        <div className="message">
+                            <p>You: Chào Giang</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="chat-list__content-user">
+                    <img src={typing} alt="avatar"/>
+                    <div className="info-message">
+                        <div className="info">
+                            <h4>Name</h4>
+                        </div>
+                        <div className="message">
+                            <p>You: Chào Giang</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+>>>>>>> Stashed changes
         </div>
     )
 }
