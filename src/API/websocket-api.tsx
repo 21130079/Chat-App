3b9d5a3cb7fdaa954f7bb682832b1b477f83f826
dat <21130022@st.hcmuaf.edu.vn>
2024-05-29 20:58:24 +0700

    add websocket api

--- src/API/websocket-api.tsx
+++ src/API/websocket-api.tsx
@@ -37,110 +37,6 @@ export const checkUser = (data: { user: string}) => {
     ws.send(JSON.stringify(action));
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 export const register = (data: { user: string,pass:string}) => {
     const action = {
         action: 'onchat',
@@ -150,6 +46,131 @@ export const register = (data: { user: string,pass:string}) => {
                 user: data.user,
                 pass: data.pass
             }
+        }
+    };
+    ws.send(JSON.stringify(action));
+}
+// Function to re-login a user
+export const reLogin = (data: { user: string, code: string }) => {
+    const action = {
+        action: 'onchat',
+        data: {
+            event: 'RE_LOGIN',
+            data: {
+                user: data.user,
+                code: data.code
+            }
+        }
+    };
+    ws.send(JSON.stringify(action));
+}
+
+// Function to logout
+export const logout = () => {
+    const action = {
+        action: 'onchat',
+        data: {
+            event: 'LOGOUT'
+        }
+    };
+    ws.send(JSON.stringify(action));
+}
+
+// Function to create a new room
+export const createRoom = (data: { name: string }) => {
+    const action = {
+        action: 'onchat',
+        data: {
+            event: 'CREATE_ROOM',
+            data: {
+                name: data.name
+            }
+        }
+    };
+    ws.send(JSON.stringify(action));
+}
+
+// Function to join an existing room
+export const joinRoom = (data: { name: string }) => {
+    const action = {
+        action: 'onchat',
+        data: {
+            event: 'JOIN_ROOM',
+            data: {
+                name: data.name
+            }
+        }
+    };
+    ws.send(JSON.stringify(action));
+}
+
+// Function to get room chat messages
+export const getRoomChatMessages = (data: { name: string, page: number }) => {
+    const action = {
+        action: 'onchat',
+        data: {
+            event: 'GET_ROOM_CHAT_MES',
+            data: {
+                name: data.name,
+                page: data.page
+            }
+        }
+    };
+    ws.send(JSON.stringify(action));
+}
+
+// Function to get people chat messages
+export const getPeopleChatMessages = (data: { name: string, page: number }) => {
+    const action = {
+        action: 'onchat',
+        data: {
+            event: 'GET_PEOPLE_CHAT_MES',
+            data: {
+                name: data.name,
+                page: data.page
+            }
+        }
+    };
+    ws.send(JSON.stringify(action));
+}
+// Function to send chat message to room
+export const sendRoomChat = (data: { to: string, mes: string }) => {
+    const action = {
+        action: 'onchat',
+        data: {
+            event: 'SEND_CHAT',
+            data: {
+                type: 'room',
+                to: data.to,
+                mes: data.mes
+            }
+        }
+    };
+    ws.send(JSON.stringify(action));
+}
+
+// Function to send chat message to a person
+export const sendPeopleChat = (data: { to: string, mes: string }) => {
+    const action = {
+        action: 'onchat',
+        data: {
+            event: 'SEND_CHAT',
+            data: {
+                type: 'people',
+                to: data.to,
+                mes: data.mes
+            }
+        }
+    };
+    ws.send(JSON.stringify(action));
+}
+
+// Function to get the user list
+export const getUserList = () => {
+    const action = {
+        action: 'onchat',
+        data: {
+            event: 'GET_USER_LIST'
         }
     };
     ws.send(JSON.stringify(action));
