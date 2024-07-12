4574fce9d8717cb3fe43caa4dc8ffc434361e180
DoanQuocDang <doanquocdang39@gmail.com>
2024-07-12 22:15:16 +0700

    feat: set background for box chat

--- src/components/ChatBox/chat-box.tsx
+++ src/components/ChatBox/chat-box.tsx
@@ -38,7 +38,7 @@ interface ChatBoxProps {
     newMessages: string[];
     setNewMessages?: (value: (((prevState: Array<string>) => Array<string>) | Array<string>)) => void;
     users: User[],
-    setSelectedImage:(value: (((prevState: (string)) => (string )) | string )) => void;
+    setSelectedImage: (value: (((prevState: (string)) => (string)) | string)) => void;
     selectedImage: string | null
 }
 
@@ -126,7 +126,7 @@ function ChatBox({
                             if (setNewMessages) {
                                 setNewMessages(prev => [response.data.name, ...prev]);
                             }
-                        }else{
+                        } else {
                             if (user?.type === 1) {
                                 getRoomChatMessages({name: user?.name, page: 1})
                             }
@@ -499,6 +499,63 @@ function ChatBox({
         }
     }
 
+    const handleSetBackGround = () => {
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.accept = 'image/*';
+
+        input.onchange = async (e) => {
+            const file = (e.target as HTMLInputElement).files?.[0];
+            if (file && file.size <= 250 * 1024) {
+                const imageBase64 = await readFileAsBase64(file);
+                localStorage.setItem(`backgroundImage_${user.name}`, imageBase64);
+                const chatBoxContent = document.querySelector('.chat-box__content') as HTMLElement;
+                chatBoxContent.style.backgroundImage = `url(${imageBase64})`;
+            } else if (file) {
+                alert("Please choose photos under 250kb in size.");
+            }
+        };
+
+        input.click();
+    };
+
+    const readFileAsBase64 = async (file: File): Promise<string> => {
+        return new Promise<string>((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onload = () => resolve(reader.result as string);
+            reader.onerror = (error) => reject(error);
+            reader.readAsDataURL(file);
+        });
+    };
+
+    const handleOpenBackground = async () => {
+        const chatBoxContent = document.querySelector('.chat-box__content') as HTMLElement;
+        const backgroundImageBase64 = localStorage.getItem(`backgroundImage_${user.name}`);
+
+        if (chatBoxContent) {
+            try {
+                if (backgroundImageBase64) {
+                    const res = await fetch(backgroundImageBase64);
+                    const blob = await res.blob();
+                    const newImageUrl = URL.createObjectURL(blob);
+                    chatBoxContent.style.backgroundImage = `url(${newImageUrl})`;
+                } else {
+                    chatBoxContent.style.backgroundImage = 'none';
+                }
+            } catch (err) {
+                console.error('Error creating blob from Base64 string:', err);
+                chatBoxContent.style.backgroundImage = 'none';
+            }
+        }
+        console.log(localStorage.length);
+    }
+
+
+    useEffect(() => {
+        if(user){
+            handleOpenBackground();
+        }
+    }, [user?.name]);
     return (
         <div className={`chat-box ${theme}`}>
             <div className="chat-box__header">
@@ -513,7 +570,10 @@ function ChatBox({
                 </div>
                 <div className="chat-box__header-icons">
                     <div className="search-box">
-                    <i className="bi bi-search" onClick={changeSearchState}></i>
+                        <i className="bi bi-search" onClick={changeSearchState}></i>
+                    </div>
+                    <div className="background-box">
+                        <i className="bi bi-file-image" onClick={handleSetBackGround}></i>
                     </div>
                     <div className="theme-box">
                         <i className="bi bi-sun-fill" onClick={toggleTheme} ref={modeIcon}></i>
